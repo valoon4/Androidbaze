2c73b011c9a10b47abc796f1fe829602253a573f
valoon4 <40501362+valoon4@users.noreply.github.com>
2026-09-14 15:28:16 +0200

    Add Ape Economics app module

---
+++ games/ApeEconomics/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.eugi.apeeconomics"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.eugi.apeeconomics"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0-debug"
+    }
+
+    buildTypes {
+        getByName("release") { isMinifyEnabled = false }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
+}
