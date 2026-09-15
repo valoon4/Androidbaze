4c3006591a7201dd4e84ac1ff3d9552cb2922d0d
valoon4 <40501362+valoon4@users.noreply.github.com>
2026-09-15 02:34:26 +0200

    Switch Ape Economics to v1.0 pixel village

--- games/ApeEconomics/app/src/main/java/com/eugi/apeeconomics/MainActivity.kt
+++ games/ApeEconomics/app/src/main/java/com/eugi/apeeconomics/MainActivity.kt
@@ -2,13 +2,15 @@ package com.eugi.apeeconomics
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : Activity() {
-    private lateinit var gameView: PixelVillageV09
+    private lateinit var gameView: PixelVillageV10
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        gameView = PixelVillageV09(this)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        gameView = PixelVillageV10(this)
         setContentView(gameView)
     }
 
